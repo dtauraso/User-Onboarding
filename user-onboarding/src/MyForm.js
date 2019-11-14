1b0814997f9e59f4634ccfc6cd303a109be59283
David Tauraso <david318@me.com>
2019-11-14 15:49:05 -0800

    Put in validation methods

--- user-onboarding/src/MyForm.js
+++ user-onboarding/src/MyForm.js
@@ -3,10 +3,12 @@ import { withFormik, Form, Field }  from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
+// values appears to be an object representing the user input as key value pairs
 const MyForm = ( {values, errors, touched, status, handleChange }) => {
 
     const [users, setUsers] = useState([])
     console.log("this is touched", touched);
+    // updating local copy of users after axios post request
     useEffect(() => {
         if(status) {
             setUsers([...users, status])
@@ -46,20 +48,30 @@ const MyForm = ( {values, errors, touched, status, handleChange }) => {
                         <p className="error">{errors.password}</p>
                         )
                 }
-                <Field
-                    type="checkbox"
-                    name="hasReadTermsOfService"
-                    checked={values.hasReadTermsOfService} />
-                {
-                    touched.hasReadTermsOfService && errors.hasReadTermsOfService && (
-                        <p className="error">{errors.hasReadTermsOfService}</p>
-                        )
-                }
+                <label>
+                    <Field
+                        type="checkbox"
+                        name="hasReadTermsOfService"
+                        checked={values.hasReadTermsOfService} />
+                    {
+                        touched.hasReadTermsOfService && errors.hasReadTermsOfService && (
+                            <p className="error">{errors.hasReadTermsOfService}</p>
+                            )
+                    }
+                    Has read terms of service
+
+                </label>
+                
                 <button>Submit</button>
             </Form>
             {users.map(user => (
                 <ul key={user.id}>
                     <li>Name: {user.name}</li>
+                    <li>Email: {user.email}</li>
+                    <li>Password: {user.password}</li>
+                    <li>Name: {String(user.hasReadTermsOfService)}</li>
+
+
                 </ul>
 
             ))}
@@ -68,6 +80,7 @@ const MyForm = ( {values, errors, touched, status, handleChange }) => {
 }
 
 const FormikUserForm = withFormik({
+    // user filling out form
     mapPropsToValues({name, email, password, hasReadTermsOfService}) {
         // holding the state of the form
         // each field can have a default "0" value or whatever the user types
@@ -79,17 +92,29 @@ const FormikUserForm = withFormik({
             hasReadTermsOfService: hasReadTermsOfService || false
         };
     },
+    // validating what the user typed in
     validationSchema: Yup.object().shape({
-        name: Yup.string().required(),
-        email: Yup.string().required(),
-        password: Yup.string().required(),
-        hasReadTermsOfService: Yup.bool().required()
+        name: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required("a name is required"),
+        email: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required("an email is required"),
+        password: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required("a password is required"),
+        hasReadTermsOfService: Yup.boolean().required("It's our way or the highway to hell!")
+
     }),
-    // same values as before
+    // send the user data to the server
+    // same values as above
     handleSubmit(values, { setStatus, resetForm}) {
         axios   .post('https://reqres.in/api/users/', values)
                 .then(res => {
-                    console.log(res.data);
+                    console.log("recieved", res.data);
                     setStatus(res.data)
                     resetForm({});
                 })
